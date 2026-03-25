5aa61afda8c0dab6f5e152000e2a91275037c280
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-26 03:22:59 +0400

    Initial commit

---
+++ src/main/java/com/example/gamematchproject/GameMatchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamematchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMatchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameMatchProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamematchproject/GameMatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamematchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameMatchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
