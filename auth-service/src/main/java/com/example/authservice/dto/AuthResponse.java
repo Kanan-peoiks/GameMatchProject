cad2e0329da4de6b71bbe2f0f0efbae545054442
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-26 12:54:38 +0400

    feat: add dto package

---
+++ auth-service/src/main/java/com/example/authservice/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.example.authservice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private Long userId;
+    private String username;
+    private String email;
+    private String role;
+}
